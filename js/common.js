22c75a94d943633b779138c83873817b5eaab7cd
imnanahee <nahee0824@gmail.com>
2023-10-04 17:05:37 +0900

    b2019 mo

---
+++ js/common.js
@@ -0,0 +1,115 @@
+const createTipContent = `
+    <span class=contClose><i class="fa-solid fa-xmark fa-xl"></i></span>
+    <dl class=tip>
+        <dt class=tip>방문관리란?</dt>
+        <dd class=tip>
+            방문관리 기간동안 코웨이 제품의 관리 전문가인 ‘코디’가 방문하여 제품별 서비스 점검시기에 방문관리 서비스를 제공합니다.
+        </dd>
+        <dt class=tip>자가관리란?</dt>
+        <dd class=tip>
+            자가관리 기간동안 제품별 필터 제공시기에 필터가 배송되면 고객님이 제품의 필터를 직접 교체하고 관리하실 수 있습니다. (일부 제품은 방문관리 전용)
+        </dd>
+    </dl>
+`;
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+    // 검색 기능 실행
+    const performSearch = () => {
+        const searchInput = document.getElementById('srch-form');
+        const searchTerm = searchInput.value.trim();
+
+        if (searchTerm === '') return;
+
+        // 상품 데이터와 검색어 일치하는 상품 찾기
+        const matchedProduct = products.find(product => product.alt.includes(searchTerm));
+        if (matchedProduct) {
+            const productURL = `./detail.html?product_id=${matchedProduct.id}`;
+            window.location.href = productURL;
+        } else {
+            alert('검색 결과가 없습니다.');
+        }
+    }
+
+
+    // nav 메뉴 클릭 시 해당되는 카테고리로 이동하는 공통 함수
+    const navigateToCategory = (index, categories) => {
+        const category  = categories[index];
+        const subURL = `${window.location.pathname}#${encodeURIComponent(category)}`;
+        window.location.href = subURL;
+        location.reload();
+    }
+
+    // 각각의 메뉴에 대한 이벤트 핸들러 등록
+    const navMenus = document.querySelectorAll('.nav-menu dl');
+    navMenus.forEach((dl, dlIndex) => {
+        const ddList = dl.querySelectorAll('dd');
+        ddList.forEach((dd, ddIndex) => {
+            dd.addEventListener('click', () => {
+                console.log(dlIndex); console.log(ddIndex);
+                const categoryLists = [['tab2', 'tab3', 'tab4', 'tab5'], ['tab2', 'tab3', 'tab4'], ['tab2', 'tab3'], ['tab2', 'tab3']];
+                const selectedCategories = categoryLists[dlIndex];
+                navigateToCategory(ddIndex, selectedCategories);
+            });
+        });
+    });
+
+    // 검색 버튼 클릭 및 Enter 키 입력 이벤트 처리
+    document.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target.classList.contains('menu')) {
+            target.nextElementSibling.classList.add('show');
+            document.documentElement.classList.add('hidden');
+            document.body.classList.add('hidden');
+        }
+
+        if (target.classList.contains('close')) {
+            target.parentElement.classList.remove('show');
+            document.documentElement.classList.remove('hidden');
+            document.body.classList.remove('hidden');
+        }
+
+        if (e.target.id === 'srch-btn') {
+            performSearch();
+        }
+
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.target.id === 'srch-form' && e.key === 'Enter') {
+            performSearch();
+        }
+    })
+});
+
+
+// header 벗어날 시 fixed 메뉴 노출
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('#header');
+    const fixedMenu = document.querySelector('.fixed-menu');
+
+    if (header && fixedMenu) {
+        const srollY = window.scrollY;
+        const headerHeight = header.clientHeight;
+    
+        if (scrollY >= headerHeight) {
+            fixedMenu.classList.add('fixed');
+        } else {
+            fixedMenu.classList.remove('fixed');
+        }
+    }
+
+})
+
+
+// 3자리마다 콤마 (가격, 렌탈가)
+const commaCheck = (e) => {
+    return e.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") 
+}
+
+// background-color Hex 방식으로 변환
+const rgbToHex = (rgb) => {
+    const values = rgb.match(/\d+/g);
+    return `#${Number(values[0]).toString(16).padStart(2, '0')}${Number(values[1]).toString(16).padStart(2, '0')}${Number(values[2]).toString(16).padStart(2, '0')}`;
+}
---
+++ js/script.js
@@ -0,0 +1,468 @@
+function loadScript(src, callback) {
+    const script = document.createElement('script');
+    script.src = src;
+  
+    script.onload = function () {
+      // 스크립트 로딩이 완료된 후 실행할 콜백 함수 호출
+      if (typeof callback === 'function') {
+        callback();
+      }
+    };
+  
+    script.onerror = function (error) {
+      // 스크립트 로딩 중 에러가 발생한 경우 처리
+      console.error('스크립트 로딩 중 에러:', error);
+    };
+  
+    document.head.appendChild(script);
+}
+
+loadScript('js/product.js', function() {
+    const products = window.products;
+
+    const body = document.querySelector('body');
+    const tabButtons = document.querySelectorAll('.category button');
+    const productContainer = document.querySelector('.product');
+    const categoryBtn = document.querySelectorAll('.category-item button');
+    const productList = document.querySelector('.product-list');
+
+
+    // product.js에서 상품 정보 가져오기
+    const productByTab = (title) => {
+        if (productContainer) {
+            productContainer.innerHTML = '';
+            const filterProduct = products.filter(product => product.title.includes(title));
+            
+            const slicedProducts = filterProduct.slice(0, 3);
+            
+            slicedProducts.forEach(product => {
+                const productElement = createProductElement(product);
+                productContainer.appendChild(productElement);
+            })
+        }
+    }
+
+    // 메인 - 베스트 상품 리스트
+    const createProductElement = (product) => {
+        const productElement = document.createElement('div');
+        productElement.classList.add('cell');
+        productElement.innerHTML = `
+            <a href='detail.html?product_id=${product.id}'>
+                <img src="${product.imgSrc}">
+                <div class=productText>
+                    <p class=desc>${product.subText}</p>
+                    <h3 class=name>${product.alt}</h3>
+                    <span class=code>${product.code}</span>
+                    <strong class=rentalPrice>렌탈가 ${commaCheck(product.rentalPrice)} 원</strong>
+                </div>
+            </a>
+        `;
+        return productElement;
+    }
+
+
+    // 서브 - 상품리스트
+    const createItemElement = (product) => {
+        const productElement = document.createElement('div');
+        productElement.classList.add('cell');
+        productElement.innerHTML = `
+            <a href='detail.html?product_id=${product.id}'>
+                <img src="${product.imgSrc}">
+                <div class=productText>
+                    <span class=category>${product.category}</span>
+                    <p class=desc>${product.subText}</p>
+                    <ul class=colorType>
+                        ${product.colors.map(colorInfo => `
+                            <li 
+                              style="background-color:${colorInfo.colorCode}"
+                              ${colorInfo.colorClass ? `class="${colorInfo.colorClass}"` : ''}
+                            >
+                                <span>${colorInfo.color}</span>
+                            </li>
+                        `).join('')}
+                    </ul>
+                    <span class=code>${product.code}</span>
+                    <h4 class=name>${product.alt}</h4>
+                    <ul class=priceWrap>
+                        <li>
+                            <p>구매</p>
+                            <span class=price>${commaCheck(product.price)}원</span>
+                        </li>
+                        <li>
+                            <p>렌탈</p>
+                            <span class=rentalPrice>${commaCheck(product.rentalPrice)}원~</span>
+                        </li>
+                    </ul>
+                </div>
+            </a>
+        `;
+
+        return productElement;
+    }
+
+    // 모든 상품 표시
+    const displayAllProducts = (category) => {
+        if (productList) {
+            productList.innerHTML = '';
+    
+            const filteredProducts = products.filter(product => product.title === category);
+            
+            filteredProducts.forEach(product => {
+                const productElement = createItemElement(product);
+                productList.appendChild(productElement);
+            });
+    
+            const categoryName = document.querySelectorAll('.category');
+            categoryName.forEach(item => {
+                if (item.textContent == '얼음정수기') {
+                    item.textContent = '냉정수기+얼음';
+                } else if (item.textContent == '가습기') {
+                    item.textContent = '가습공기청정기'
+                }
+            });
+        }
+    }
+
+    const displayProducts = (category) => {
+        productList.innerHTML = '';
+
+        const filteredProducts = products.filter(product => product.category === category);
+
+        filteredProducts.forEach(product => {
+            const productElement = createItemElement(product);
+            productList.appendChild(productElement);
+        });
+        
+        const categoryName = document.querySelector('.category');
+        if (category == '얼음정수기') {
+            categoryName.textContent = '냉정수기+얼음';
+        } else if (category == '가습기') {
+            categoryName.textContent = '가습공기청정기'
+        }
+    }
+
+    // 메인 - 코웨이 BEST 상품 탭메뉴
+    tabButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            tabButtons.forEach(tabButton => {
+                tabButton.classList.remove('on');
+            });
+
+            button.classList.add('on');
+
+            const title = button.textContent;
+            productByTab(title);
+
+            const tabId = button.id;
+            productContainer.className = `product ${tabId}`;
+        })
+    })
+
+    // 상품리스트 탭메뉴
+    categoryBtn.forEach(button => {
+        button.addEventListener('click', () => {
+            const selectedCategory = button.textContent;
+
+            if (selectedCategory === '전체') {
+                if (body.classList.contains('water-page')) {
+                    displayAllProducts('정수기');
+                } else if (body.classList.contains('air-page')) {
+                    displayAllProducts('청정기');
+                } else if (body.classList.contains('body-page')) {
+                    displayAllProducts('비데/연수기');
+                } else if (body.classList.contains('sleep-page')) {
+                    displayAllProducts('매트리스/프레임');
+                }
+            } else {
+                displayProducts(selectedCategory); // 카테고리에 해당되는 상품 노출
+            }
+
+            categoryBtn.forEach(btn => btn.classList.remove('on'));
+            button.classList.add('on');
+        })
+    })
+
+    if (body.classList.contains('index-page')) {
+        productByTab(tabButtons[0].textContent);
+    }
+
+    // displayProducts(), categoryBtn() 모듈화
+    const handleTab = (tabId, categoryName) => {
+        categoryBtn.forEach(button => {
+            button.classList.toggle('on', button.id === tabId);
+        });
+        displayProducts(categoryName);
+    }
+    
+    const hash = window.location.hash;
+    if (body.classList.contains('water-page')) { // 정수기 페이지
+        displayAllProducts('정수기');
+        
+        if (hash.includes('tab2')) {
+            handleTab('tab2', '냉정수기');
+        } else if (hash.includes('tab3')) {
+            handleTab('tab3', '얼음정수기');
+        } else if (hash.includes('tab4')) {
+            handleTab('tab4', '냉온정수기');
+        } else if (hash.includes('tab5')) {
+            handleTab('tab5', '정수전용');
+        }
+        
+    } else if (body.classList.contains('air-page')) { // 청정기 페이지
+        displayAllProducts('청정기');
+
+        if (hash.includes('tab2')) {
+            handleTab('tab2', '공기청정기');
+        } else if (hash.includes('tab3')) {
+            handleTab('tab3', '가습기');
+        } else if (hash.includes('tab4')) {
+            handleTab('tab4', '의류청정기');
+        }
+
+    } else if (body.classList.contains('body-page')) { // 비데 페이지
+        displayAllProducts('비데/연수기');
+
+        if (hash.includes('tab2')) {
+            handleTab('tab2', '비데');
+        } else if (hash.includes('tab3')) {
+            handleTab('tab3', '연수기');
+        }
+
+    } else if (body.classList.contains('sleep-page')) { // 매트리스 페이지
+        displayAllProducts('매트리스/프레임');
+
+        if (hash.includes('tab2')) {
+            handleTab('tab2', '매트리스');
+        } else if (hash.includes('tab3')) {
+            handleTab('tab3', '프레임');
+        }
+    }
+})
+
+// const script = document.createElement('script');
+// script.src = 'js/product.js';
+
+// script.onload = () => {
+//     const products = window.products;
+
+//     const body = document.querySelector('body');
+//     const tabButtons = document.querySelectorAll('.category button');
+//     const productContainer = document.querySelector('.product');
+//     const categoryBtn = document.querySelectorAll('.category-item button');
+//     const productList = document.querySelector('.product-list');
+
+
+//     // product.js에서 상품 정보 가져오기
+//     const productByTab = (title) => {
+//         if (productContainer) {
+//             productContainer.innerHTML = '';
+//             const filterProduct = products.filter(product => product.title.includes(title));
+            
+//             const slicedProducts = filterProduct.slice(0, 3);
+            
+//             slicedProducts.forEach(product => {
+//                 const productElement = createProductElement(product);
+//                 productContainer.appendChild(productElement);
+//             })
+//         }
+//     }
+
+//     // 메인 - 베스트 상품 리스트
+//     const createProductElement = (product) => {
+//         const productElement = document.createElement('div');
+//         productElement.classList.add('cell');
+//         productElement.innerHTML = `
+//             <a href='detail.html?product_id=${product.id}'>
+//                 <img src="${product.imgSrc}">
+//                 <div class=productText>
+//                     <p class=desc>${product.subText}</p>
+//                     <h3 class=name>${product.alt}</h3>
+//                     <span class=code>${product.code}</span>
+//                     <strong class=rentalPrice>렌탈가 ${commaCheck(product.rentalPrice)} 원</strong>
+//                 </div>
+//             </a>
+//         `;
+//         return productElement;
+//     }
+
+
+//     // 서브 - 상품리스트
+//     const createItemElement = (product) => {
+//         const productElement = document.createElement('div');
+//         productElement.classList.add('cell');
+//         productElement.innerHTML = `
+//             <a href='detail.html?product_id=${product.id}'>
+//                 <img src="${product.imgSrc}">
+//                 <div class=productText>
+//                     <span class=category>${product.category}</span>
+//                     <p class=desc>${product.subText}</p>
+//                     <ul class=colorType>
+//                         ${product.colors.map(colorInfo => `
+//                             <li 
+//                               style="background-color:${colorInfo.colorCode}"
+//                               ${colorInfo.colorClass ? `class="${colorInfo.colorClass}"` : ''}
+//                             >
+//                                 <span>${colorInfo.color}</span>
+//                             </li>
+//                         `).join('')}
+//                     </ul>
+//                     <span class=code>${product.code}</span>
+//                     <h4 class=name>${product.alt}</h4>
+//                     <ul class=priceWrap>
+//                         <li>
+//                             <p>구매</p>
+//                             <span class=price>${commaCheck(product.price)}원</span>
+//                         </li>
+//                         <li>
+//                             <p>렌탈</p>
+//                             <span class=rentalPrice>${commaCheck(product.rentalPrice)}원~</span>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </a>
+//         `;
+
+//         return productElement;
+//     }
+
+//     // 모든 상품 표시
+//     const displayAllProducts = (category) => {
+//         if (productList) {
+//             productList.innerHTML = '';
+    
+//             const filteredProducts = products.filter(product => product.title === category);
+            
+//             filteredProducts.forEach(product => {
+//                 const productElement = createItemElement(product);
+//                 productList.appendChild(productElement);
+//             });
+    
+//             const categoryName = document.querySelectorAll('.category');
+//             categoryName.forEach(item => {
+//                 if (item.textContent == '얼음정수기') {
+//                     item.textContent = '냉정수기+얼음';
+//                 } else if (item.textContent == '가습기') {
+//                     item.textContent = '가습공기청정기'
+//                 }
+//             });
+//         }
+//     }
+
+//     const displayProducts = (category) => {
+//         productList.innerHTML = '';
+
+//         const filteredProducts = products.filter(product => product.category === category);
+
+//         filteredProducts.forEach(product => {
+//             const productElement = createItemElement(product);
+//             productList.appendChild(productElement);
+//         });
+        
+//         const categoryName = document.querySelector('.category');
+//         if (category == '얼음정수기') {
+//             categoryName.textContent = '냉정수기+얼음';
+//         } else if (category == '가습기') {
+//             categoryName.textContent = '가습공기청정기'
+//         }
+//     }
+
+//     // 메인 - 코웨이 BEST 상품 탭메뉴
+//     tabButtons.forEach(button => {
+//         button.addEventListener('click', () => {
+//             tabButtons.forEach(tabButton => {
+//                 tabButton.classList.remove('on');
+//             });
+
+//             button.classList.add('on');
+
+//             const title = button.textContent;
+//             productByTab(title);
+
+//             const tabId = button.id;
+//             productContainer.className = `product ${tabId}`;
+//         })
+//     })
+
+//     // 상품리스트 탭메뉴
+//     categoryBtn.forEach(button => {
+//         button.addEventListener('click', () => {
+//             const selectedCategory = button.textContent;
+
+//             if (selectedCategory === '전체') {
+//                 if (body.classList.contains('water-page')) {
+//                     displayAllProducts('정수기');
+//                 } else if (body.classList.contains('air-page')) {
+//                     displayAllProducts('청정기');
+//                 } else if (body.classList.contains('body-page')) {
+//                     displayAllProducts('비데/연수기');
+//                 } else if (body.classList.contains('sleep-page')) {
+//                     displayAllProducts('매트리스/프레임');
+//                 }
+//             } else {
+//                 displayProducts(selectedCategory); // 카테고리에 해당되는 상품 노출
+//             }
+
+//             categoryBtn.forEach(btn => btn.classList.remove('on'));
+//             button.classList.add('on');
+//         })
+//     })
+
+//     if (body.classList.contains('index-page')) {
+//         productByTab(tabButtons[0].textContent);
+//     }
+
+//     // displayProducts(), categoryBtn() 모듈화
+//     const handleTab = (tabId, categoryName) => {
+//         categoryBtn.forEach(button => {
+//             button.classList.toggle('on', button.id === tabId);
+//         });
+//         displayProducts(categoryName);
+//     }
+    
+//     const hash = window.location.hash;
+//     if (body.classList.contains('water-page')) { // 정수기 페이지
+//         displayAllProducts('정수기');
+        
+//         if (hash.includes('tab2')) {
+//             handleTab('tab2', '냉정수기');
+//         } else if (hash.includes('tab3')) {
+//             handleTab('tab3', '얼음정수기');
+//         } else if (hash.includes('tab4')) {
+//             handleTab('tab4', '냉온정수기');
+//         } else if (hash.includes('tab5')) {
+//             handleTab('tab5', '정수전용');
+//         }
+        
+//     } else if (body.classList.contains('air-page')) { // 청정기 페이지
+//         displayAllProducts('청정기');
+
+//         if (hash.includes('tab2')) {
+//             handleTab('tab2', '공기청정기');
+//         } else if (hash.includes('tab3')) {
+//             handleTab('tab3', '가습기');
+//         } else if (hash.includes('tab4')) {
+//             handleTab('tab4', '의류청정기');
+//         }
+
+//     } else if (body.classList.contains('body-page')) { // 비데 페이지
+//         displayAllProducts('비데/연수기');
+
+//         if (hash.includes('tab2')) {
+//             handleTab('tab2', '비데');
+//         } else if (hash.includes('tab3')) {
+//             handleTab('tab3', '연수기');
+//         }
+
+//     } else if (body.classList.contains('sleep-page')) { // 매트리스 페이지
+//         displayAllProducts('매트리스/프레임');
+
+//         if (hash.includes('tab2')) {
+//             handleTab('tab2', '매트리스');
+//         } else if (hash.includes('tab3')) {
+//             handleTab('tab3', '프레임');
+//         }
+//     }
+    
+// }
+
+// document.head.appendChild(script);
